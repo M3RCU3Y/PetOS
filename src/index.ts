51c3c5dff4f4b2fd0de79c46584ed90fe11c2188
Brandon  Deonarine <brandondeonarine123@gmail.com>
2026-08-21 00:13:35 -0400

    feat(core): expose PetOS simulation API

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from "./core/types.js";
+export * from "./core/rng.js";
+export * from "./core/species.js";
+export * from "./core/memory.js";
+export * from "./core/brain.js";
+export * from "./core/pet.js";
+export * from "./core/world.js";
